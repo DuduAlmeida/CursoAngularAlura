af00c3f52dd84bfd461784332d2955f19421e335
Eduardo Almeida <contato.eduardopex@hotmail.com>
2021-01-04 11:50:55 -0300

    Validando o formulário

--- alurapic/src/app/photos/photo-form/photo-form.component.ts
+++ alurapic/src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,44 @@
+import { Validators } from '@angular/forms';
+/* #region Imports*/
+
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+
+/* #endregion Imports*/
 
 @Component({
   selector: 'ap-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
 })
 export class PhotoFormComponent implements OnInit {
 
-  constructor() { }
+  /* #region Constructor*/
 
-  ngOnInit() {
+  /*** 
+   * Construtor padrão
+   */
+  constructor(
+    private readonly formBuilder: FormBuilder
+  ) { }
+
+  /* #endregion Constructor*/
+
+  /* #region LifeCycle Events*/
+
+  ngOnInit(): void {
+    this.photoForm = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
+
+  /* #endregion LifeCycle Events*/
+
+  /* #region Public Properties*/
+
+  photoForm: FormGroup;
+
+  /* #endregion Public Properties*/
 
 }
